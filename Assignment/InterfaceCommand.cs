d5052052f25dcf4b67a1fab81932acc56b040573
GwGibson <gibsonwgraham@gmail.com>
2023-06-08 19:59:52 -0400

    Revert to base assignment

--- Assignment/InterfaceCommand.cs
+++ Assignment/InterfaceCommand.cs
@@ -4,38 +4,3 @@ public interface RobotCommand
 {
     void Run(Robot robot); // Interfaces are public and abstract by default.
 }
-
-public class OffCommand : RobotCommand
-{
-    public void Run(Robot robot) => robot.IsPowered = false;
-}
-
-public class OnCommand : RobotCommand
-{
-    public void Run(Robot robot) => robot.IsPowered = true;
-}
-
-public class WestCommand : RobotCommand
-{
-    public void Run(Robot robot) { if (robot.IsPowered) robot.X--; }
-}
-
-public class EastCommand : RobotCommand
-{
-    public void Run(Robot robot) { if (robot.IsPowered) robot.X++; }
-}
-
-public class SouthCommand : RobotCommand
-{
-    public void Run(Robot robot) { if (robot.IsPowered) robot.Y--; }
-}
-
-public class NorthCommand : RobotCommand
-{
-    public void Run(Robot robot) { if (robot.IsPowered) robot.Y++; }
-}
-
-// Implement remaining tests (easy & fast)
-// Write docs for Run & LoadCommand methids in Robot class (easyish and fast)
-// Write your own RobotCommand 
-// (Optional) If you have time you can write a RobotTester class as described instructions
--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -1,5 +1,5 @@
 ﻿using Assignment;
-using Assignment.InterfaceCommand; // Change to Assignment.InterfaceCommand when rdy
+using Assignment.AbstractCommand; // Change to Assignment.InterfaceCommand when rdy
 
 namespace AssignmentTest
 {
@@ -29,66 +29,5 @@ namespace AssignmentTest
             robot1.Y = -5;
             Assert.AreEqual(robot1.Y, -5);
         }
-
-        [TestMethod]
-        public void OnCommandTest()
-        {
-            Robot testRobot = new Robot();
-            Assert.AreEqual(testRobot.IsPowered, false);
-            testRobot.LoadCommand(new OnCommand());
-            testRobot.Run();
-            Assert.AreEqual(testRobot.IsPowered, true);
-            // Robot executes the OnCommand again (memory isn't cleared between runs)
-            testRobot.Run();
-            // Verifies if a turned on Robot is turned on again, it stays powered on
-            Assert.AreEqual(testRobot.IsPowered, true);
-        }
-
-        [TestMethod]
-        public void OffCommandTest()
-        {
-            Robot testRobot = new();
-            Assert.AreEqual(testRobot.IsPowered, false);
-            testRobot.IsPowered = true;
-            testRobot.LoadCommand(new OffCommand());
-            testRobot.Run();
-            Assert.AreEqual(testRobot.IsPowered, false);
-            testRobot.Run();
-            Assert.AreEqual(testRobot.IsPowered, false);
-        }
-
-        [TestMethod]
-        public void MoveWestCommandTest()
-        {
-            Robot testRobot = new Robot();
-            Assert.AreEqual(testRobot.X, 0);
-            testRobot.LoadCommand(new WestCommand());
-            testRobot.Run();
-            // Robot is powered off by default
-            Assert.AreEqual(testRobot.X, 0); // Robot should not move if it isn't turned on
-            testRobot.IsPowered = true;
-            testRobot.Run();
-            Assert.AreEqual(testRobot.X, -1);
-            testRobot.Run();
-            Assert.AreEqual(testRobot.X, -2);
-        }
-
-        [TestMethod]
-        public void MoveNorthCommandTest()
-        {
-            Robot testRobot = new Robot();
-            Assert.AreEqual(testRobot.Y, 0);
-            testRobot.LoadCommand(new NorthCommand());
-            testRobot.Run();
-            // Robot is powered off by default
-            Assert.AreEqual(testRobot.Y, 0); // Robot should not move if it isn't turned on
-            testRobot.IsPowered = true;
-            testRobot.Run();
-            Assert.AreEqual(testRobot.Y, 1);
-            testRobot.Run();
-            Assert.AreEqual(testRobot.Y, 2);
-        }
-
-        // Implement South & East Commands Tests (derivative of existing ones)
     }
 }
